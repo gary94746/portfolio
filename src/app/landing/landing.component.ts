540d530efba48b27c450e77c8b83546660ecc26e
gary94746 <gary.109885@gmail.com>
2020-09-27 00:11:32 -0500

    fix: scroll to id

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -23,4 +23,28 @@ export class LandingComponent implements OnInit {
   landingInfo() {
     return combineLatest([this.service.userName(), this.service.title()]);
   }
+
+  goAbout() {
+    this.toggleClass();
+    const element = document.getElementById('about');
+    element.scrollIntoView();
+  }
+
+  goSkills() {
+    this.toggleClass();
+    const element = document.getElementById('skills');
+    element.scrollIntoView();
+  }
+
+  goProjects() {
+    this.toggleClass();
+    const element = document.getElementById('projects');
+    element.scrollIntoView();
+  }
+
+  goExperience() {
+    this.toggleClass();
+    const element = document.getElementById('experience');
+    element.scrollIntoView();
+  }
 }
